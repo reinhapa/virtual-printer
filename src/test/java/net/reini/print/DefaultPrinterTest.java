56be041db40bfc556253c62dfe2c433e2818a101
Patrick Reinhart <patrick@reini.net>
2018-03-20 23:07:07 +0100

    initial commit

--- src/test/java/net/reini/print/DefaultPrinterTest.java
+++ src/test/java/net/reini/print/DefaultPrinterTest.java
@@ -1,7 +1,7 @@
 package net.reini.print;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
